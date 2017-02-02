6c149d5a7f040f76240460c0f2b43d20429d7a4a
leonawicz <matt.rstat@gmail.com>
2017-02-02 09:18:18 -0700

    Convert to dashboard layout, add rintrojs, add tour and map modal buttons, add empty About tab.

--- standage/ui.R
+++ standage/ui.R
@@ -1,6 +1,9 @@
 library(shiny)
+library(shinydashboard)
+library(rintrojs)
 library(leaflet)
 
+action_btn_style <- "margin: 10px 15px 10px 15px; width: 200px"
 reg <- c(
   "DNR"="State Department of Natural Resources",
   "BLM"="Bureau of Land Management (BLM)",
@@ -29,37 +32,64 @@ text_dist2 <- "These estimated annual probability distributions can be integrate
 For each period here, 30 annual distributions are integrated to show stand age densities at a coarser time scale.
 These density plots are included to provide context regarding where mean stand age estimates and uncertainty in the other two plots are derived from."
 
-shinyUI(fluidPage(
-  title="TPA stand age",
-  fluidRow(column(12, h3("Terrestrial protected areas stand age summary"))),
-  tabsetPanel(
-    tabPanel("Mean", plotOutput("plotmean", height="auto")),
-    tabPanel("95% confidence", plotOutput("plotci", height="auto")),
-    tabPanel("Example distributions", plotOutput("plotdist", height="auto")),
-    id="tsp"
+dashboardPage(
+  dashboardHeader(
+    title="Terrestrial protected areas stand age summary",
+    titleWidth=450,
+    tags$li(class="dropdown",
+            tags$a(href="http://snap.uaf.edu", target="_blank",
+                   tags$img(src="SNAP_acronym_100px.png", width="100%", alt="SNAP"), style="margin: 10px; padding: 0px;")
+    )
+    #tags$head(includeScript("ga-nwtapp.js"), includeScript("ga-allapps.js")),
   ),
-  fluidRow(
-    column(2, selectInput("reg", "Area", reg, reg[1], multiple=TRUE, width="100%")),
-    column(2, selectInput("veg", "Vegetation", veg, width="100%")),
-    conditionalPanel("input.tsp == 'Example distributions'",
-      column(2, selectInput("fctby", "Facet by", c("Region"="Location", "Period"), width="100%"))),
-    conditionalPanel("input.tsp != 'Example distributions'",
-      column(2, selectInput("x", "X-axis", c("Year", "Decade"), width="100%"))),
-    column(2, selectInput("fctscales", "Axis scales", c("Fixed"="fixed", "Free"="free_y"), width="100%")),
-    conditionalPanel("input.tsp == 'Example distributions'",
-      column(2, sliderInput("adj", "Density smoothing", 0.2, 2, 1, 0.2, width="100%"))),
-    conditionalPanel("input.tsp != 'Example distributions'",
-      conditionalPanel("input.tsp == 'Mean'",
-        column(2, selectInput("pos", "Bars", c("Stacked"="stack", "Proportions"="fill"), width="100%"))
+  dashboardSidebar(
+    introjsUI(),
+    sidebarMenu(
+      id="tabs",
+      menuItem("Stand age", icon=icon("sliders"), tabName="age"),
+      menuItem("About", icon=icon("sliders"), tabName="about")
+    ),
+    actionButton("help", "Take tour", style=action_btn_style, class="btn-flat action-button btn-block", icon=icon("question-circle")),
+    actionButton("map", "View map", style=action_btn_style, class="btn-flat action-button btn-block", icon=icon("globe"))
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName="age",
+        tabBox(
+          tabPanel("Example distributions", plotOutput("plotdist", height="auto")),
+          tabPanel("95% confidence", plotOutput("plotci", height="auto")),
+          tabPanel("Mean", plotOutput("plotmean", height="auto")),
+          id="tb", selected="Mean", title="Stand age summaries", width=12, side="right"
+        ),
+        fluidRow(
+          column(2, selectInput("reg", "Area", reg, reg[1], multiple=TRUE, width="100%")),
+          column(2, selectInput("veg", "Vegetation", veg, width="100%")),
+          conditionalPanel("input.tb == 'Example distributions'",
+            column(2, selectInput("fctby", "Facet by", c("Region"="Location", "Period"), width="100%"))),
+          conditionalPanel("input.tb != 'Example distributions'",
+            column(2, selectInput("x", "X-axis", c("Year", "Decade"), width="100%"))),
+          column(2, selectInput("fctscales", "Axis scales", c("Fixed"="fixed", "Free"="free_y"), width="100%")),
+          conditionalPanel("input.tb == 'Example distributions'",
+            column(2, sliderInput("adj", "Density smoothing", 0.2, 2, 1, 0.2, width="100%"))),
+          conditionalPanel("input.tb != 'Example distributions'",
+            conditionalPanel("input.tb == 'Mean'",
+              column(2, selectInput("pos", "Bars", c("Stacked"="stack", "Proportions"="fill"), width="100%"))
+            ),
+            column(2, sliderInput("span", "Series smoothing", 0, 1, 0, 0.1, width="100%"))
+          )
+        ),
+        fluidRow(
+          column(12,
+            conditionalPanel("input.tb == 'Mean'", p(em(text_mean))),
+            conditionalPanel("input.tb == '95% confidence'", p(em(text_ci))),
+            conditionalPanel("input.tb == 'Example distributions'", p(em(text_dist1)), p(em(text_dist2)))
+          )
+        )
       ),
-      column(2, sliderInput("span", "Series smoothing", 0, 1, 0, 0.1, width="100%"))
+      tabItem(tabName="about",
+              "Nothing here yet..."
+      )
     )
   ),
-  fluidRow(
-    column(12,
-      conditionalPanel("input.tsp == 'Mean'", p(em(text_mean))),
-      conditionalPanel("input.tsp == '95% confidence'", p(em(text_ci))),
-      conditionalPanel("input.tsp == 'Example distributions'", p(em(text_dist1)), p(em(text_dist2)))
-    )
-  )
-))
+  title="TPA stand age"
+)
